37d3a64c6113c238d7d6da718df1c53a7172997c
Diana Soltani <31975705+DianaSoltani@users.noreply.github.com>
2018-04-12 20:48:51 -0400

    Update play.py

--- play.py
+++ play.py
@@ -1,5 +1,5 @@
 from pygame import mixer # Load the required library
 
 mixer.init()
-mixer.music.load('songTest/Slushii_-_LUV_U_NEED_U.mp3')
+mixer.music.load('songTest/song.mp3')
 mixer.music.play()
